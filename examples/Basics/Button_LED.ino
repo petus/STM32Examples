9332ef1acd1373c1b0ef8099a34cb416c9f2ebe2
petus <petus@users.noreply.github.com>
2018-02-15 16:49:42 +0100

    Create Button_LED.ino

---
+++ examples/Basics/Button_LED.ino
@@ -0,0 +1,44 @@
+/*
+*  Button and LED with Nucleo 64 STM32F411 which is supported in Arduino IDE by
+*  www.github.com/stm32duino
+*
+*  Description:
+*  If you connect the PC11 with GND, LED is blinking slowly
+*  If you connect the PC11 with 3V3, LED is blinking faster
+*
+*  Connection:
+*  Anode of LED is connected to PC10, Cathode of LED to GND
+*  Button is connected between PC11 and GND
+*
+*  Made by Petr Sramek (2018)
+*  https://www.linkedin.com/in/petr-%C5%A1r%C3%A1mek-7b0072107/
+*/
+
+
+// the setup function runs once when you press reset or power the board
+void setup() 
+{
+    // initialize digital pin PC10 as an output.
+    pinMode(PC10, OUTPUT);
+    // initialize digital pin PC11 as an input.
+    pinMode(PC11, INPUT);
+}
+
+// the loop function runs over and over again forever
+void loop() 
+{
+    if(digitalRead(PC11))		    // read button
+    {
+        digitalWrite(PC10, HIGH);   // turn the LED on (HIGH is the voltage level)
+        delay(100);                 // wait 100ms
+        digitalWrite(PC10, LOW);    // turn the LED off by making the voltage LOW
+        delay(100);                 // wait 100ms
+    }
+    else
+    {
+        digitalWrite(PC10, HIGH);   // turn the LED on (HIGH is the voltage level)
+        delay(500);                 // wait 500ms
+        digitalWrite(PC10, LOW);    // turn the LED off by making the voltage LOW
+        delay(500);                 // wait 500ms
+    }
+}
