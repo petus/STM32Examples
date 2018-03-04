b9d9017593565669623fcb548699a7ca719fd418
petus <petus@users.noreply.github.com>
2018-03-04 16:55:44 +0100

    Update Serial.ino

--- examples/Communication/Serial.ino
+++ examples/Communication/Serial.ino
@@ -9,8 +9,7 @@
 *  Connection:
 *  -
 *
-*  Made by Petr Sramek (2018)
-*  https://www.linkedin.com/in/petr-%C5%A1r%C3%A1mek-7b0072107/
+*  Made by http://stm32duino.eu (2018)
 */
 // the setup function runs once when you press reset or power the board
 void setup() 
